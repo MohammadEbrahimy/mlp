b2be95685fc8089123d42ff859a0dbbe33a8d6a3
MohammadEbrahimy <mohammaddxgi@gmail.com>
2020-05-31 11:10:59 +0430

    normalizing and backpropagtion

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -18,18 +18,78 @@ class MlpClassifier(object):
         for i in range(self.layer_number):
             self.weights.append(np.random.rand(layers["Layer"+str(i)]+1,
                                                layers["Layer"+str(i+1)]))
-
+            self.weights[i][-1, 1:] = self.weights[i][-1, 0]
     # Activation function
     def sigmoid(self, X):
         return 1 / (1+np.exp(-X))
 
+    # Step function for output layer
+    def step_function(self, output):
+        if output >= 0:
+            return 1
+        else:
+            return 0
+    
+    # Normalizing Data
+    def normalize(self, x):
+        return (x-min(x))/(max(x)-min(x))
+
     # FeedForward
     def forward(self, X):
-        self.layers["Layer0"] = X.shape[0]
         X = np.append(X, 1)
-        self.initialize_parameters(self.layers)
+        h = [X]
         for i in range(self.layer_number):
             X = self.weights[i].T @ X
             X = self.sigmoid(X)
             X = np.append(X, 1)
-        return X
+            h.append(X)
+        return X[0], h
+
+    #BackPropagation
+    def back_propagation(self, y, y_hat, h, learning_rate):
+        error_out = abs(y_hat*(1-y_hat)*(y-y_hat))
+        # print('y is :', y)
+        # print('y_hat is :', y_hat)
+        # print("error is :", error_out)
+        end_layer = True
+        Dw = []
+        dh = []
+        Sum = []
+        dh.append(np.array([error_out]))
+        for i in reversed(range(len(self.weights)+1)):
+            if end_layer:
+                my_sum = self.weights[i-1][:-1] * error_out
+                Sum.append(my_sum)
+                dh.append(np.array([(h[i][:-1]*(1-h[i][:-1]))]).T * my_sum)
+                # dw = ([number * dh[0] for number in h[i-1]])
+                # Dw.append(np.array([[number * learning_rate for number in dw]]))
+                end_layer = False
+            else:
+                if i != 0:
+                    w = self.weights[i-1][:-1]
+                    for e in range(w.shape[1]): 
+                        temp_sum = w @ dh[-1]
+                        for n in  reversed(range(len(dh)-1)):
+                            for j in range(len(dh[n])):
+                                temp_sum[j][0] += dh[n][j]
+                    if i != 1:
+                        dh_temp = np.array([(h[i-1][:-1]*(1-h[i-1][:-1]))]).T * temp_sum
+                        dh.append(np.array([dh_temp.sum(axis=1, dtype='float')]).T)
+                    dw = (h[i] * dh[abs(i-(len(self.weights)-1))]).T
+                    # print("-----------------------------------------------")
+                    Dw.append(np.array([[number * learning_rate for number in dw]]))
+                else:
+                    dw = (h[i] * dh[abs(i-(len(self.weights)-1))]).T
+                    Dw.append(np.array([[number * learning_rate for number in dw]]))
+        # print(Dw[0])
+        # print(Dw[1])
+        # print(Dw[2])
+        # print(Dw[3])
+
+    def predict(self, x, y, learning_rate):
+        self.layers["Layer0"] = x.shape[1]
+        self.initialize_parameters(self.layers)
+        for i in range(y.shape[0]):
+            x_normal = self.normalize(x[i, :])
+            y_hat, h = self.forward(x_normal)
+            self.back_propagation(y[i], y_hat, h, learning_rate)
