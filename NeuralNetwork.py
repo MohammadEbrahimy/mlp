8288cd063dd0abbb7474aef1461e167363c11899
MohammadEbrahimy <mohammaddxgi@gmail.com>
2020-06-01 17:01:25 +0430

    Add predict function and MSE

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from sklearn.metrics import accuracy_score
 
 class MlpClassifier(object):
 
@@ -23,46 +23,54 @@ class MlpClassifier(object):
     def sigmoid(self, X):
         return 1 / (1+np.exp(-X))
 
+
+    # Normalizing Data
+    def normalize(self, x):
+        return (x-min(x))/(max(x)-min(x))
+    
+    def MSE(self, all_y , all_y_hat):
+        # print(all_y)
+        # print(all_y_hat)
+        mse = np.sum(((all_y - all_y_hat) ** 2)) / len(all_y)
+
+        return mse
+
     # Step function for output layer
     def step_function(self, output):
         if output >= 0:
             return 1
         else:
             return 0
-    
-    # Normalizing Data
-    def normalize(self, x):
-        return (x-min(x))/(max(x)-min(x))
 
     # FeedForward
-    def forward(self, X):
+    def forward(self, X, learn_or_predict):
         X = np.append(X, 1)
         h = [X]
         for i in range(self.layer_number):
             X = self.weights[i].T @ X
-            X = self.sigmoid(X)
+            if learn_or_predict == 1 and i == self.layer_number-1:
+                X = self.step_function(X) 
+            else:
+                X = self.sigmoid(X)
             X = np.append(X, 1)
             h.append(X)
         return X[0], h
 
+
     #BackPropagation
     def back_propagation(self, y, y_hat, h, learning_rate):
-        error_out = abs(y_hat*(1-y_hat)*(y-y_hat))
-        # print('y is :', y)
-        # print('y_hat is :', y_hat)
-        # print("error is :", error_out)
+        error_out = y_hat * (1 - y_hat) * (y - y_hat)
         end_layer = True
         Dw = []
         dh = []
         Sum = []
+        
         dh.append(np.array([error_out]))
         for i in reversed(range(len(self.weights)+1)):
             if end_layer:
                 my_sum = self.weights[i-1][:-1] * error_out
                 Sum.append(my_sum)
                 dh.append(np.array([(h[i][:-1]*(1-h[i][:-1]))]).T * my_sum)
-                # dw = ([number * dh[0] for number in h[i-1]])
-                # Dw.append(np.array([[number * learning_rate for number in dw]]))
                 end_layer = False
             else:
                 if i != 0:
@@ -76,20 +84,44 @@ class MlpClassifier(object):
                         dh_temp = np.array([(h[i-1][:-1]*(1-h[i-1][:-1]))]).T * temp_sum
                         dh.append(np.array([dh_temp.sum(axis=1, dtype='float')]).T)
                     dw = (h[i] * dh[abs(i-(len(self.weights)-1))]).T
-                    # print("-----------------------------------------------")
-                    Dw.append(np.array([[number * learning_rate for number in dw]]))
+                    Dw.append(np.array([number * learning_rate for number in dw]))
                 else:
                     dw = (h[i] * dh[abs(i-(len(self.weights)-1))]).T
-                    Dw.append(np.array([[number * learning_rate for number in dw]]))
-        # print(Dw[0])
-        # print(Dw[1])
-        # print(Dw[2])
-        # print(Dw[3])
+                    Dw.append(np.array([number * learning_rate for number in dw]))
+        return Dw
 
-    def predict(self, x, y, learning_rate):
+
+    def update_weights(self, dw):
+        for i in range(len(dw)):
+            dw[0] = np.array([dw[0]]).T
+            self.weights[len(dw)-(1+i)] += dw[i]
+    
+    def predict(self, X, Y):
+        y_hats = []
+        for i in range(X.shape[0]):
+            x_normal = self.normalize(X[i, :])
+            y, h = self.forward(x_normal, 1)
+            y_hats.append(y)
+        mse = MSE = self.MSE(Y, y_hats)
+        accuracy = accuracy_score(Y, y_hats)
+        print('MSE : ', mse)
+        print('Accuracy : ', accuracy)
+
+
+    def learn(self, x, y, learning_rate, ephoc):
         self.layers["Layer0"] = x.shape[1]
         self.initialize_parameters(self.layers)
-        for i in range(y.shape[0]):
-            x_normal = self.normalize(x[i, :])
-            y_hat, h = self.forward(x_normal)
-            self.back_propagation(y[i], y_hat, h, learning_rate)
+        j = 0
+        while j <= ephoc:
+            all_y_hat = []
+            for i in range(y.shape[0]):
+                x_normal = self.normalize(x[i, :])
+                y_hat, h = self.forward(x_normal, 0)
+                all_y_hat.append(y_hat)
+                dw = self.back_propagation(y[i], y_hat, h, learning_rate)
+                self.update_weights(dw)
+            j += 1
+            MSE = self.MSE(y, all_y_hat)
+            print("-----------------------------------------------")
+            print(MSE)
+        
--- main.py
+++ main.py
@@ -6,18 +6,22 @@ import NeuralNetwork as NN
 train = pd.read_csv("dataset/nba_logreg.csv", header=None)
 test = pd.read_csv("dataset/test.csv", header=None)
 
+train = train.dropna()
+test = test.dropna()
+
 # Split the train and test data
 x_train = train.values[1:, 1:20].astype(np.float)
 y_train = train.values[1:, 20].astype(np.float)
 x_test = test.values[1:, 2:21].astype(np.float)
 y_test = test.values[1:, 21].astype(np.float)
 
-test = NN.MlpClassifier()
-test.add_layer(5)
-test.add_layer(3)
-test.add_layer(2)
-test.add_layer(1)
-# a = test.forward(x_train[1, :])
-test.predict(x_train, y_train, 0.001)
+mlp = NN.MlpClassifier()
+mlp.add_layer(10)
+mlp.add_layer(15)
+mlp.add_layer(5)
+mlp.add_layer(2)
+mlp.add_layer(1)
 
-# print(a)
+mlp.learn(x_train, y_train, 0.08, 500)
+mlp.predict(x_test, y_test)
+
